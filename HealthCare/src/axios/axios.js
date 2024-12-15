d9aa5e8b70126fbedc7f171d749ac12776021003
Gagan V <191381956gaganctvs@gmail.com>
2024-12-15 21:59:04 +0530

    Added additional error checks in case the token from local storage is null

--- HealthCare/src/axios/axios.js
+++ HealthCare/src/axios/axios.js
@@ -4,16 +4,20 @@ const axiosInstance = axios.create({
    baseURL: 'http://localhost:8080/',
    headers: {
      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
+     'Content-Type': 'application/json',
       withCredentials: true
    },
 });
 
 axiosInstance.interceptors.request.use(request => {
-      const access_token = localStorage.getItem('access_token').slice(1, -1);
+      const localStorageAccessToken = localStorage.getItem('access_token');
+      let access_token = null;
+      if(localStorageAccessToken) {
+         access_token = localStorage.getItem('access_token').slice(1, -1);
+      }
       console.log(access_token)
       if (access_token) {
-      request.headers['Authorization'] = `Bearer ${access_token}`;
+         request.headers['Authorization'] = `Bearer ${access_token}`;
       }
       return request;
    }, error => {
