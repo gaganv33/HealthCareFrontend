ae30cee018016fb16d619a0b050802e93c9fc62d
Gagan V <191381956gaganctvs@gmail.com>
2025-03-13 23:01:26 +0530

    Minor changes

--- HealthCare/src/Components/HomePageComponent/HomePage.jsx
+++ HealthCare/src/Components/HomePageComponent/HomePage.jsx
@@ -1,7 +1,7 @@
 function HomePage() {
    return (
       <p>
-         Healthcare analytics involves the collection and analysis of data within the healthcare sector to improve patient outcomes, operational efficiency, and cost management. By leveraging data from electronic health records, medical devices, and other sources, healthcare organizations can make informed decisions through predictive, descriptive, and prescriptive analytics. Similarly, an inventory management application is a software tool designed to track, manage, and optimize stock levels in real-time. It ensures efficient inventory control, automates reordering, enhances demand forecasting, and integrates with other systems to streamline operations across various industries such as retail, healthcare, and manufacturing. Both healthcare analytics and inventory management systems aim to drive better decision-making and operational efficiency in their respective fields.
+         
       </p>
    );
 }
